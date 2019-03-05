12ab1aa38fb374daf0020024b18bdbb1f866765f
DeeYeum <zdr2780@gmail.com>
2019-03-05 18:11:14 +0000

    Week 4 exercises updated

--- JavaSmallExercises/weeklyExercises/src/week4/ArrayFront9.java
+++ JavaSmallExercises/weeklyExercises/src/week4/ArrayFront9.java
@@ -1,7 +1,18 @@
 
 class ArrayFront9{
-	public static void main(String[] args){
-		
+	public static boolean arrayFront9(int[] nums){
+		for (int n : nums){
+			if (n == 9) return true;
+		}
+		return false;
 		
 	}
+	public static void main(String[] args){
+		int N = args.length;
+		int[] nums = new int[N];
+		for (int n = 0; n < N; n++){
+			nums[n] = Integer.parseInt(args[n]);
+		}
+		System.out.println(arrayFront9(nums));
+	}	
 }
