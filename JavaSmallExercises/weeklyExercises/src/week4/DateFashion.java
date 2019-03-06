5a74a2483a8601fd6d11d34425187de4d2168ac0
Dee Yeum <s1769931@sardar.inf.ed.ac.uk>
2019-03-06 14:13:13 +0000

    DateFashion.java started

---
+++ JavaSmallExercises/weeklyExercises/src/week4/DateFashion.java
@@ -0,0 +1,16 @@
+
+
+public static int dateFashion(int you, int date)
+{
+	/* If either of you is very stylish, that is, 8 or more, then the result is 2 (yes), except that if either of you has style of 2 or less, then the result is 0 (no). Otherwise the result is 1 (maybe). Here are some examples of evaluating the method on representative input values:
+	if (you > )
+
+
+
+}
+
+public static void main (String[] args)
+{
+	
+
+}
