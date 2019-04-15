cf81b92453a69dd7c72845fbd1bfd0e35d52139a
DeeYeum <zdr2780@gmail.com>
2019-04-15 18:29:45 +0100

    LeetCode added

---
+++ LeetCode/860.py
@@ -0,0 +1,28 @@
+"""
+860. Lemonade Change
+
+At a lemonade stand, each lemonade costs $5. 
+
+Customers are standing in a queue to buy from you, and order one at a time (in the order specified by bills).
+
+Each customer will only buy one lemonade and pay with either a $5, $10, or $20 bill.  You must provide the correct change to each customer, so that the net transaction is that the customer pays $5.
+
+Note that you don't have any change in hand at first.
+
+Return true if and only if you can provide every customer with correct change.
+
+"""
+
+
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        total = 0
+        for x in bills:
+            if x == d:
+                 
+
+
+
+
+if __name__ == "__main__":
+      
