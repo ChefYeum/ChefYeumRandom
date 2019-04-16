7169c768bf9cdefa1208d041232d7bbe19af163a
DeeYeum <zdr2780@gmail.com>
2019-04-16 18:42:12 +0100

    Leetcode problem 763 started

---
+++ LeetCode/763.py
@@ -0,0 +1,25 @@
+
+
+
+class Solution(object):
+    def partitionLabels(self, S):
+        """
+        :type S: str
+        :rtype: List[int]
+        """
+        """
+        1. Find all unique letters
+        2. For each letter, find range within the input string
+        3. Merge overlapped ranges
+        4. Split it accordingly and return 
+        """
+        
+        uniqueLetters = [ : !
+
+
+if __name__ == "__main__":
+
+    def try(inputStr):
+        s = Solution()
+        s.partitionLabels("abcde")
+
