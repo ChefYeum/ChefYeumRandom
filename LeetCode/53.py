1219772e1eef67a27064a73928361c1df558d319
DeeYeum <zdr2780@gmail.com>
2019-04-28 01:17:00 +0100

    LeetCode update

---
+++ LeetCode/53.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        pass
+
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print (s.maxSubArray([-2,1,-3,4,-1,2,1,-5,4] == 6))
+
