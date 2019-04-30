b518d99cf9221405da9f0232358b225495d2af8a
Dee Yeum <chefyeum@gmail.com>
2019-04-30 12:46:28 +0100

    Inf1oop1617mock done

---
+++ Inf1OOP/inf1oop1617mock/src/ComplexNumbers.java
@@ -0,0 +1,78 @@
+
+
+import java.util.*;
+
+public class ComplexNumbers {
+	public static double[] complexAdd(double[] z1, double[] z2) {
+		return new double[] {(z1[0]+z2[0]),(z1[1]+z2[1])};
+	}
+	
+
+	
+	public static double[] complexConjugate(double[] z) {
+		return new double[] {z[0],-z[1]};
+	}
+	
+	public static double[] complexMultiply(double[] z1, double[] z2) {
+		double a = z1[0];
+		double b = z1[1];
+		double c = z2[0];
+		double d = z2[1];
+		return new double[] {(a*c-b*d),(b*c+a*d)};
+	}
+	
+	public static double[] complexReciprocal(double[] z) {
+		double a = z[0];
+		double b = z[1];
+		return new double[] {(a/(Math.pow(a,2)+Math.pow(b, 2))), (b/(Math.pow(a, 2)+Math.pow(b, 2)))};
+	}
+	
+	public static String toString(double[] z) {
+		double a = (Math.round(z[0]*10))/10.0;
+		double b = (Math.round(z[1]*10))/10.0;
+		String sign = b < 0 ? "-" : "+"; 
+		return (a+sign+Math.abs(b)+"i");
+	}
+	
+	private static double magnitude(double[] z) {
+		if (Objects.isNull(z)) return -1;
+		else {
+			double a = z[0];
+			double b = z[1];
+			return Math.sqrt(Math.pow(a, 2)+Math.pow(b, 2));
+		}
+	}
+	
+	
+	public static double[][] sortByMagnitude(double[][] complexList){
+		double[][] outputArr = new double[complexList.length][];
+		
+		for (int i = complexList.length-1; i >= 0; i--) {
+			int maxPos = 0;
+			for (int j = 1; j < complexList.length; j++) {
+				if (magnitude(complexList[j]) >= magnitude(complexList[maxPos])) {
+					maxPos = j;
+				}
+			}
+			outputArr[i] = complexList[maxPos];
+			complexList[maxPos]= null;
+		}
+		
+		return outputArr;
+	}
+	
+	public static void main(String[] args) {
+		double[] z1 = new double[] {Double.parseDouble(args[0]), Double.parseDouble(args[1])};
+		double[] z2 = new double[] {Double.parseDouble(args[2]), Double.parseDouble(args[3])};
+		
+		System.out.println("("+toString(complexAdd(z1,z2))+")");
+		System.out.println("("+toString(complexConjugate(z1))+")");
+		System.out.println("("+toString(complexMultiply(z1,z2))+")");
+		System.out.println("("+toString(complexReciprocal(z1))+")");
+		System.out.println("("+toString(complexConjugate(complexAdd(z1,z2)))+")");
+
+		
+		
+	}
+}
+
---
+++ Inf1OOP/inf1oop1617mock/src/Student.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Student extends Person {
+
+	private ArrayList<Course> activeCourses; //stores the courses that the student has enrolled
+	private ArrayList<Course> completedCourses;
+	private int maxCourses; //number of active courses that the student can be enrolled into
+	
+	public Student(String firstName, String lastName,
+					ArrayList<Course> activeCourses,
+					ArrayList<Course> completedCourses,
+					int maxCourses) { 
+		super(firstName, lastName);
+		this.activeCourses = activeCourses;
+		this.completedCourses = completedCourses;
+		this.maxCourses = maxCourses;
+	}
+	
+	
+	public void setMaxCourses(int n) {this.maxCourses = n;}
+	public int getMaxCourses() {return this.maxCourses;}
+	
+	public boolean canEnroll(Course course) {
+		//Check all prereqs are satisfied
+		ArrayList<String> completed = new ArrayList<>();
+		for (Course completedCourse : this.completedCourses) completed.add(completedCourse.getName());
+		
+		for (String preReq: course.getPrerequisiteCourses()) {
+			if (!completed.contains(preReq)) return false;
+		}
+		
+		//Check if the student is not already enrolled, or if it is already completed, or if the student exceeded the max number of courses
+		if (activeCourses.contains(course)
+			|| completedCourses.contains(course)
+			|| activeCourses.size() >= maxCourses) return false;
+		else return true;
+	}
+	
+	public boolean enroll(Course course) {
+		if (this.canEnroll(course)) {
+			activeCourses.add(course);
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public boolean complete(String courseName) {
+		for (Course course : this.activeCourses) {
+			if (course.getName() == courseName) {
+				this.completedCourses.add(course);
+				this.activeCourses.remove(course);
+				return true;
+			}
+		}
+		return false; //otherwise.
+	}
+	
+	public HashMap<Course,String> getAllCourses(){
+		HashMap<Course,String> outputMap = new HashMap<>();
+		
+		//put all the active courses in the output map
+		for (Course course : this.activeCourses) {
+			outputMap.put(course, "active")
+		}
+		
+		//put all the completed courses in the output map
+		for (Course course : this.completedCourses) {
+			outputMap.putAll(course, "completed");
+		}
+		
+		return outputMap;
+	}
+	
+	public String summary() {
+		StringBuilder output = new StringBuilder();
+		
+		for (Course course : this.activeCourses) {
+			output.append(String.format("%s (active)", course.getName()).toString());
+		}
+	
+		for (Course course : this.completedCourses) {
+			output.append(String.format("%s (completed)", course.getName().toString()));
+		}
+		
+		return output.toString();
+	
+	}
+}
