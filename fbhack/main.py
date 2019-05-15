0c035af1566515b7770ec7a8ac81d2cc5ab2ab5a
Dee Yeum <chefyeum@gmail.com>
2019-05-15 21:10:56 +0100

    fbhack modified

--- fbhack/main.py
+++ fbhack/main.py
@@ -1,35 +1,52 @@
+
 def countSteps(piles):
     output = 0
     lastCount = 0
     nums = (sorted(list(set(piles)),reverse=True)[:-1])
-    print (nums)
     for nextMax in nums:
         lastCount = piles.count(nextMax) + lastCount
         output += lastCount 
     return output 
 
-def counterSteps(piles):
+def minimumMoves(q):
     from collections import Counter
-    c = Counter(piles)
-    output = 0
-    piles = sorted(c.items(), key=lambda pair: pair[0], reverse=True)
-    piles.pop()
-    for n in piles:
-        output = 2*output+n[1]
-    return output
-        
-def check(f):
-    from random import randint
-    for repeat in range(10):
-        n = randint(0,2*(10**5)) 
-        piles = [randint(0,10**6) for x in range(n)] 
-    print ("Done")
+    C = Counter(q)
+    p = sorted(C.items(), key=lambda pair: pair[0], reverse=True)
+    p.pop()
+    ret = 0
+    lastCount = 0
+    for (_,b) in p:
+        ret += lastCount + b
+        lastCount += b
+    return ret
 
 def quickCheck(f1,f2):
     from random import randint
-    for repeat in range(1000):
-        n = randint(1,(10)) 
-        piles = [randint(1,2*10) for x in range(n)] 
+    for repeat in range(2):
+        n = randint(1,2*(10**4))
+        piles = [randint(1,2*(10**5)) for x in range(n)] 
+        
+        
+        result1 = f1(piles[:])
+        result2 = f2(piles[:])
+        
+        print ("FP: ", result1)
+        print ("OOP: ", result2)
+
+
+        if result1 != result2:
+            print ("Falsified by %s and %s!\n" % (n, piles))
+            return False 
+        else:
+            print ("Proved true by %s and %s!\n" % (n, piles))
+    else:
+        return True
+
+def quickCheck(f1,f2):
+    from random import randint
+    for repeat in range(10):
+        n = randint(1,2*(10**5))
+        piles = [randint(1,2*(10**5)) for x in range(n)] 
         
         result1 = f1(piles)
         result2 = f2(piles)
@@ -47,7 +64,8 @@ def quickCheck(f1,f2):
         return True
 
 if __name__ == "__main__":
-    #quickCheck(counterSteps,countSteps)
+    print (quickCheck(minimumMoves,countSteps))
     dummy = [17, 17, 19, 7, 7, 14]
-    print (countSteps(dummy))
+    dummy = [13,13,7,7,5,6,3]
+    #print (countSteps(dummy))
     
