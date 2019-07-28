caa765bf6d52a959db7a95ef1f8cda4a44421ccc
ChefYeum <chefyeum@gmail.com>
2019-07-28 12:40:19 +0100

    Two new problems added

---
+++ LeetCode/491.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print (findSubsequences([4,6,7,7]))
