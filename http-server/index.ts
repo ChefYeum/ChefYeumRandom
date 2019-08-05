a10fd7e995d2e98d73c527534e2ad39066ccb5ac
ChefYeum <chefyeum@gmail.com>
2019-08-05 12:00:22 +0100

    Minimum HTTP server done

--- http-server/index.ts
+++ http-server/index.ts
@@ -1,20 +1,12 @@
 import * as http from 'http';
-import * as readline from 'readline';
 
-const server = http.createServer((req,res) => {
-    console.log(req.statusCode);
+let port = 4444;
+let hostname = "localhost";
+
+const server = http.createServer((req,_) => {
+    console.log(req.headers);
 });
 
-const stdinout = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-let port;
-const hostname = "localhost";
-
-stdinout.question('Please specify the port: ', (p) => {
-    port = p;
-    server.listen(port,  );
-
-});
+server.listen(port, hostname, () =>{
+    console.log(`Server running at http://${hostname}:${port}/`)
+})
