fd933b748ec02609421380bbe50ffb22a2f25e06
ChefYeum <chefyeum@gmail.com>
2019-08-06 23:28:52 +0100

    Added Array parser for LinkedList

--- CtCI/LinkedList/LinkedList.java
+++ CtCI/LinkedList/LinkedList.java
@@ -4,6 +4,14 @@ public class LinkedList<T1>{
         this.head = node;
     } 
     
+    public LinkedList(Array<T1> values){
+        Node<T1> current = head;
+        for (int i = 0; i < values.length(); i++){
+            current = new Node(values[i])
+            current = current.next;
+        }
+    }
+
     public String toString(){
         StringBuilder output = new StringBuilder();
         Node<T1> current = this.head;
