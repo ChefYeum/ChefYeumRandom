a0e6de114ced8d12c73d4d30fbd1ca7a894302ee
ChefYeum <chefyeum@gmail.com>
2019-11-24 00:36:44 +0000

    4.2 Minimal Tree completed

--- CtCI/Trees+Graphs/TreesGraphs.py
+++ CtCI/Trees+Graphs/TreesGraphs.py
@@ -1,14 +1,52 @@
-class Graph:
-    def __init__(self, children):
-        self.children = children
+from typing import List
 
 class Node:
     def __init__(self, val, children = []):
         self.val = val
         self.children = children 
 
+class BinaryTreeNode:
+    def __init__(self, val, left = None, right = None):
+        self.val = val
+        self.left: Node = left 
+        self.right: Node = right
+    def __str__(self):
+        output = f"{self.val}\n"
+        currentLevel = [self.left, self.right] 
+
+        while currentLevel:
+            nextLevel = []
+            for node in currentLevel:
+                if node:
+                    output += str(node.val)
+                    output += " "
+                    nextLevel.append(node.left)
+                    nextLevel.append(node.right)
+                else:
+                    output += "* "
+
+
+            output += "\n"
+            currentLevel = nextLevel
+
+        return output
     def __repr__(self):
-        pass
+        return str(self.val)
+
+class LinkedListNode:
+    def __init__(self, val, next = None):
+        self.val = val
+        self.next = next
+    def append(self, val):
+        if self.next:
+            self.next.append(val)
+        else:
+            self.next = LinkedListNode(val)
+    def __repr__(self):
+        if self.next:
+            return f"{self.val} -> " + repr(self.next)
+        else:
+            return f"{self.val}"
 
 class TreesGraphs:
     @staticmethod
@@ -42,5 +80,18 @@ class TreesGraphs:
         else:
             return False
 
+    """
+    Given a sorted array with unique integer elements, write an algorithm to create a binary search tree with minimal height
+    """
     @staticmethod
-    def minimalTree
+    def minimalTree(ns: List) -> BinaryTreeNode:
+        if len(ns) == 0:
+            return None
+        elif len(ns) == 1:
+            return BinaryTreeNode(ns[0])
+        else:
+            midPos = len(ns)//2
+            leftSubTree = TreesGraphs.minimalTree(ns[:midPos])
+            rightSubTree = TreesGraphs.minimalTree(ns[midPos+1:])
+            return BinaryTreeNode(ns[midPos],leftSubTree,rightSubTree)
+    
